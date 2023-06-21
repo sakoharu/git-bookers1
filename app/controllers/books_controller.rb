61ef86ea40492ead64781043d67de28ea87aaa74
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-06-21 10:52:41 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -38,6 +38,7 @@ class BooksController < ApplicationController
     # redirect_to book_path(book.id)
     @book = Book.find(params[:id])
     if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -46,8 +47,9 @@ class BooksController < ApplicationController
   end
 
   def destroy
-　　book = Book.find(params[:id])
-　　book.destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
